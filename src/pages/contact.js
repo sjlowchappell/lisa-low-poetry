024656f01237c0d42bd2667467342ce2c70e77a9
Sam Low-Chappell <sam@hackeryou.com>
2020-04-04 12:58:20 -0400

    added content to publications and contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const Contact = () => (
-    <Layout>
-        <SEO title="Contact" />
-        <h1>Contact</h1>
-        <p>In progress</p>
-    </Layout>
-);
-
+const Contact = () => {
+    const formSubmit = e => {
+        e.preventDefault();
+    };
+    return (
+        <Layout>
+            <SEO title="Contact" />
+            <h1>Contact</h1>
+            <p>You can contact me at lisalowwrites@gmail.com</p>
+            <form action="" onSubmit={formSubmit}>
+                <label htmlFor="name">Name:</label>
+                <input id="name" type="text" />
+                <label htmlFor="email">Email:</label>
+                <input id="email" type="email" />
+                <label htmlFor="message">Message</label>
+                <textarea
+                    name="message"
+                    id="message"
+                    cols="30"
+                    rows="10"
+                ></textarea>
+                <button>Submit</button>
+            </form>
+        </Layout>
+    );
+};
 export default Contact;
