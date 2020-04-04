55fffa7da2ca7088c9b166b37fd43f78d13f1c38
Sam Low-Chappell <sam@hackeryou.com>
2020-04-04 13:28:54 -0400

    adding some color

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,10 @@ const IndexPage = () => (
             <h1>Lisa Low Website</h1>
             <p>Welcome to your new site.</p>
             <p>In progress.</p>
+            <div className="red1"></div>
+            <div className="blue1"></div>
+            <div className="red2"></div>
+            <div className="blue2"></div>
         </div>
     </Layout>
 );
