6b9218e13c4bf8d1c91f747dbf4f8b42a61b49b0
Sam Low-Chappell <sam@hackeryou.com>
2020-04-04 12:09:40 -0400

    adding site pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,30 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+    <header>
+        <div>
+            <h1>
+                <Link to="/">{siteTitle}</Link>
+            </h1>
+            <nav>
+                <Link to="/about/">About</Link>
+                <Link to="/poems/">Poems</Link>
+                <Link to="/publications/">Publications</Link>
+                <Link to="/contact/">Contact</Link>
+                <Link to="/blog/">Blog</Link>
+            </nav>
+        </div>
+    </header>
+);
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+    siteTitle: PropTypes.string,
+};
 
 Header.defaultProps = {
-  siteTitle: ``,
-}
+    siteTitle: ``,
+};
 
-export default Header
+export default Header;
