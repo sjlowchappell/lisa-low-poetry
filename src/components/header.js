91515f9e941b68f9a0f0216bb00929a10f0ea228
Sam Low-Chappell <sam@hackeryou.com>
2020-04-05 11:38:04 -0400

    updated styling

--- src/components/header.js
+++ src/components/header.js
@@ -2,23 +2,25 @@ import { Link } from 'gatsby';
 import React from 'react';
 import styles from './header.module.css';
 
-const Header = () => (
-    <header className={styles.header}>
-        <div className="wrapper">
-            <div className={styles.headerContainer}>
-                <h1>
-                    <Link to="/">LL</Link>
-                </h1>
-                <nav>
-                    <Link to="/about/">About</Link>
-                    <Link to="/poems/">Poems</Link>
-                    <Link to="/publications/">Publications</Link>
-                    <Link to="/contact/">Contact</Link>
-                    <Link to="/blog/">Blog</Link>
-                </nav>
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className="wrapper">
+                <div className={styles.headerContainer}>
+                    <h1>
+                        <Link to="/">LL</Link>
+                    </h1>
+                    <nav>
+                        <Link to="/about/">About</Link>
+                        <Link to="/poems/">Poems</Link>
+                        <Link to="/publications/">Publications</Link>
+                        <Link to="/contact/">Contact</Link>
+                        <Link to="/blog/">Blog</Link>
+                    </nav>
+                </div>
             </div>
-        </div>
-    </header>
-);
+        </header>
+    );
+};
 
 export default Header;
