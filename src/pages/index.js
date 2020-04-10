2805bd910306734f7ed8fb7c716e35dc55091fff
Sam Low-Chappell <sam@hackeryou.com>
2020-04-10 19:29:57 -0400

    added carousel slider to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
-
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import styles from './index.module.css';
@@ -40,16 +41,58 @@ const IndexPage = () => (
         <div className="wrapper">
             <div className={styles.homeSection}>
                 <h2 className={styles.headings}>Excerpts</h2>
-                <div>
-                    <p>
-                        “A cargo of roses washing up on a shore; forty hands
-                        held out to touch me, could not be more beautiful than
-                        she is tonight, her eyes reflecting light, turning the
-                        mantelpiece, the window, the curtains, and the night.”
-                    </p>
-                    <p>“Driving to Hotels for Lunch, the Day My Father Died”</p>
-                    <p>Phoebe: A Journey of Literary Arts</p>
-                </div>
+
+                <Carousel
+                    showStatus={false}
+                    infiniteLoop
+                    autoPlay
+                    interval={9000}
+                    transitionTime={1000}
+                >
+                    <div className={styles.excerpt}>
+                        <p>
+                            “A cargo of roses washing up on a shore; forty hands
+                            held out to touch me, could not be more beautiful
+                            than she is tonight, her eyes reflecting light,
+                            turning the mantelpiece, the window, the curtains,
+                            and the night.”
+                        </p>
+                        <p>
+                            “Driving to Hotels for Lunch, the Day My Father
+                            Died”
+                        </p>
+                        <p>Phoebe: A Journey of Literary Arts</p>
+                    </div>
+                    <div className={styles.excerpt}>
+                        <p>
+                            “A cargo of roses washing up on a shore; forty hands
+                            held out to touch me, could not be more beautiful
+                            than she is tonight, her eyes reflecting light,
+                            turning the mantelpiece, the window, the curtains,
+                            and the night.”
+                        </p>
+                        <p>
+                            “Driving to Hotels for Lunch, the Day My Father
+                            Died”
+                        </p>
+                        <p>Excerpt 2</p>
+                    </div>
+                    <div className={styles.excerpt}>
+                        <p>
+                            “A cargo of roses washing up on a shore; forty hands
+                            held out to touch me, could not be more beautiful
+                            than she is tonight, her eyes reflecting light,
+                            turning the mantelpiece, the window, the curtains,
+                            and the night.”
+                        </p>
+                        <p>
+                            “Driving to Hotels for Lunch, the Day My Father
+                            Died”
+                        </p>
+                        <p>Excerpt 3</p>
+                    </div>
+                </Carousel>
+
                 <Link className={styles.readMore} to="/excerpts/">
                     Read More
                 </Link>
