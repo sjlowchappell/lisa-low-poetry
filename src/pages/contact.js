8c82268720dfe3ced480d1d17ea48a73947a648a
Sam Low-Chappell <sam@hackeryou.com>
2020-04-12 13:56:46 -0400

    updated contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,71 +4,61 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import styles from './contact.module.css';
 
-const Contact = () => {
-    const formSubmit = e => {
-        e.preventDefault();
-    };
-    return (
-        <Layout>
-            <SEO title="Contact" />
-            <div className="wrapper">
-                <div>
-                    <h1 className="heading">Contact</h1>
-                </div>
-                <div className={styles.contactContainer}>
-                    <p>
-                        Lisa Low can be reached at lisalowwrites@gmail.com.
-                        Alternatively, you can fill out the form below if you
-                        want to get in contact or learn more.
-                    </p>
-                    <form
-                        name="contact"
-                        action="/submit"
-                        method="POST"
-                        data-netlify="true"
-                    >
-                        <input type="hidden" name="bot-field" />
-                        <input type="hidden" name="form-name" value="contact" />
-                        <div className={styles.nameAndEmailContainer}>
-                            <div className={styles.inputContainer}>
-                                <label htmlFor="name">Name</label>
-                                <input
-                                    name="name"
-                                    type="text"
-                                    id="name"
-                                    required
-                                />
-                            </div>
-                            <div className={styles.inputContainer}>
-                                <label htmlFor="email">Email</label>
-                                <input
-                                    name="email"
-                                    type="email"
-                                    id="email"
-                                    required
-                                />
-                            </div>
-                        </div>
-
-                        <div className={styles.inputContainer}>
-                            <label htmlFor="message">Message</label>
-                            <textarea
-                                name="message"
-                                id="message"
-                                rows="6"
-                                required
-                            ></textarea>
-                        </div>
-
-                        <div className={styles.buttonContainer}>
-                            <button type="submit" className={styles.button}>
-                                Submit
-                            </button>
-                        </div>
-                    </form>
-                </div>
+const Contact = () => (
+    <Layout>
+        <SEO title="Contact" />
+        <div className="wrapper">
+            <div>
+                <h1 className="heading">Contact</h1>
             </div>
-        </Layout>
-    );
-};
+            <div className={styles.contactContainer}>
+                <p>
+                    Lisa Low can be reached at lisalowwrites@gmail.com.
+                    Alternatively, you can fill out the form below if you want
+                    to get in contact or learn more.
+                </p>
+                <form
+                    name="contact"
+                    action="/submit"
+                    method="POST"
+                    data-netlify="true"
+                >
+                    <input type="hidden" name="bot-field" />
+                    <input type="hidden" name="form-name" value="contact" />
+                    <div className={styles.nameAndEmailContainer}>
+                        <div className={styles.inputContainer}>
+                            <label htmlFor="name">Name</label>
+                            <input name="name" type="text" id="name" required />
+                        </div>
+                        <div className={styles.inputContainer}>
+                            <label htmlFor="email">Email</label>
+                            <input
+                                name="email"
+                                type="email"
+                                id="email"
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    <div className={styles.inputContainer}>
+                        <label htmlFor="message">Message</label>
+                        <textarea
+                            name="message"
+                            id="message"
+                            rows="6"
+                            required
+                        ></textarea>
+                    </div>
+
+                    <div className={styles.buttonContainer}>
+                        <button type="submit" className={styles.button}>
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </Layout>
+);
 export default Contact;
