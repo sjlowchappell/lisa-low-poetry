802615f50ee0472f9a5d996eba6d727e43766eb7
Sam Low-Chappell <sam@hackeryou.com>
2020-04-13 19:49:17 -0400

    updated publications

---
+++ src/utils/publicationsContent.js
@@ -0,0 +1,64 @@
+export const poetryForthcoming = [
+    `“On the Night You Were Born,” <span class="publication">Spillway</span> (forthcoming)`,
+    `“Blunt Force,” <span class="publication">Streetlight Magazine</span> (forthcoming)`,
+    `“Absent Parents,” <span class="publication"> Evening Street Review</span> (forthcoming)`,
+    `
+    “Doing a Good Job at Sixty,” <span class="publication"> Evening Street Review </span> (forthcoming)`,
+    `“Iowa Heat,” <span class="publication"> Evening Street Review </span> (forthcoming)`,
+    `“Floating---for Eva, After Joan Miro,” <span class="publication">Tusclulum Review</span>, (forthcoming)`,
+    `“In Love with Mike,” <span class="publication">Broken Plate</span>, (forthcoming)`,
+    `“Adultery,” <span class="publication">Broken Plate</span>, (forthcoming)`,
+    `“By Some God Meticulously Kept,” <span class="publication">Delmarva Review</span>, Delmarva Review (forthcoming)`,
+];
+export const poetryPublished = [
+    `“A Snowy Day,” <span class="publication">BoomerLitMag</span>, Volume V, No. 2`,
+    `“Poet in New York,” <span class="publication">BoomerLitMag</span>, Volume V, No. 2`,
+    `“Couples, Green with Envy,” <span class="publication">Litbreak Magazine</span>, March 19, 2020`,
+    `“On a Farm,” <span class="publication">Litbreak Magazine</span>, March 19, 2020`,
+    `“Cut Back,” <span class="publication">Litbreak Magazine</span>, March 19, 2020`,
+    `“Make a Glad Noise, It is Spring,” <span class="publication">Litbreak Magazine</span>,  March 19, 2020`,
+    `“Love Poem to Oswald,” <span class="publication">Potomac Review</span>, #66, Spring, 2020`,
+    `“On Throwing Away My Daughter’s Christmas Card Photo,” <span class="publication">Potomac Review</span>, #66, Spring, 2020.`,
+    `“The Genesis of Mrs. Dalloway,” <span class="publication">Crack the Spine</span>, #259, January 27, 2020`,
+    `“Driving to Hotels for Lunch the Day My Father Died,” <span class="publication">Phoebe: A Journal of Literary Arts</span>, Winter, 1994`,
+];
+export const anthologizedPoems = [
+    `“Summer,” <span class="publication">We Will Not Be Silenced</span>,  eds. Christine Ray et. al., Indie Blue Publications, 2018.`,
+    `“First Boyfriend,” <span class="publication"> We Will Not Be Silenced </span>, eds. Christine Ray, et. al., Indie Blue Publications, 2018.`,
+    `“Her Party,” <span class="publication">Intro 11</span>, gen. ed. Tony Ardizzone, Associated Writing Programs, National Endowment for the Humanities, (Norfolk, VA, 1980), p. 47-49.`,
+];
+export const books = [
+    `<span class="publication">Virginia Woolf & Literary History: Part I</span>, eds. Jane Lilienfeld, Jeffrey Oxford, and Lisa Low, <span class="publication">Woolf Studies Annual</span>, Vol. 9 (NYC: Pace UP, 2003).`,
+    `<span class="publication">Milton, the Metaphysicals, and Romanticism</span>, eds. Lisa Low and Anthony John Harding (Cambridge: Cambridge UP, 1994)`,
+];
+export const scholarlyWorks = [
+    `“Woolf’s Allusion to Comus in <span class="publication">The Voyage Out</span>, in <span class="publication">Milton and Gender</span>, ed. Catherine Gimelli Martin (Cambridge UP, 2004), pp. 254-70.`,
+    `“Feminist Elegy/Feminist Prophecy: Lycidas, The Waves, Kristeva, and Cixous in Virginia Woolf,” in <span class="publication">Virginia Woolf and Literary History, Part I</span>, eds. Jane Lilienfeld, Jeffrey Oxford, and Lisa Low, <span class="publication">Woolf Studies Annual</span>, Vol. 9 (NYC: Pace UP, 2003), pp. 221-42.`,
+    `“Thou Canst Not Touch the Freedom of My Mind: Fascism, Disruption, and Female Consciousness in <span class="publication">Mrs. Dalloway</span>, in <span class="publication">Virginia Woolf and Fascism</span>, ed. Merry M. Pawlowski (London: Palgrave, 2001), pp. 75-91.`,
+    `“Listen and Save: Woolf’s Allusion to Comus in Her Revolutionary First Novel” in <span class="publication">Reading the Renaissance</span>, ed. Sally Greene (Athens, Ohio: Ohio UP, 1999), pp. 117-35.`,
+    `“Refusing to Hit Back: Virginia Woolf and the Impersonality Question,” in <span class="publication">Virginia Woolf and the Essay</span>, eds. Beth Carole Rosenberg and Jeanne Dubino (NYC: St. Martin’s Press, 1997), pp. 257-74.`,
+    `“Two Figures in Dense Violet Night: Virginia Woolf, John Milton, and the Epic Vision of Man,” in <span class="publication">Woolf Studies Annual</span>, Volume 1 (NYC: Pace UP, 1995), pp. 68-88.`,
+    `“Reading the Past, Reflecting Present,” Lisa Low and Anthony John Harding in<span class="publication">Milton, the Metaphysicals, and Romanticism</span>, eds. Lisa Low and Anthony John Harding (Cambridge UP 1994), pp. 1-19.`,
+    `“Marvell Through Keats and Stevens: The Early Modern Meditation Poem,” in <span class="publication">Milton, the Metaphysicals, and Romanticism</span>, eds. Lisa Low and Anthony John Harding (Cambridge UP 1994), pp. 241-57.`,
+    `“Woolf’s Literary Heritage: Reconciling the Sexes: Milton and Woolf in the Women’s Studies and English Literary Classroom,” in <span class="publication">Re-Reading, Re-Writing, Re-Teaching Virginia Woolf</span>, eds. Eileen Barrett and Patricia Cramer (NY: Pace UP, 1994), pp. 92-95.`,
+    `In Defense of Hedda,” <span class="publication">Drama Criticism</span>, Vol. 2, Ed. Lawrence Trudeau (Detroit and London: Gale Research, 1992), pp. 350-53.`,
+    `“Ridding Ourselves of Macbeth,” in <span class="publication">Macbeth</span>, ed. Harold Bloom (NYC: Chelsea House, 1991), pp. 211-20.`,
+    `“Ridding Ourselves of Macbeth,” in <span class="publication">Massachusetts Review</span>, Vol. XXIV, No. 4 (U Mass P, Amherst: 1983), pp. 826-37.`,
+    `“In Defense of Hedda,” <span class="publication">Massachusetts Studies in English</span>, Vol. VIII, No. 3 (U Mass P, Amherst: 1982), pp. 43-49.`,
+];
+export const interviews = [
+    `“Interview with Susan Sontag,” in <span class="publication">Crosscurrents: A Yearbook of Central European Culture</span>, Crosscurrents: A Yearbook of Central European Culture (Ann Arbor: U of Michigan P, 1989), pp. 185-94.`,
+    `Excerpts from “Free Association: A Conversation with John Cage,” <span class="publication">Conversing with Cage</span>, ed. Richard Kostelanetz (New York: Limelight Editions, 1987).`,
+    `“Free Association: A Conversation with John Cage,” <span class="publication">Boston Review</span> (Boston, MA: 1985).`,
+    `Multiple Theatre Reviews and Interviews with Playwrights, Actors, and Writers on Christian Science Broadcasting Television and Radio.`,
+];
+export const reviews = [
+    `<span class="publication"> Word of Mouth: Body Language in Katherine Mansfield and Virginia Woolf </span>,  by Patricia Moran in <span class="publication">Woolf Studies Annual</span>, Vol. 4 (Pace UP: 1998), pp. 246-49.`,
+    `<span class="publication">>, The Politics of the Essay: Feminist Perspectives</span>, eds. Ruth Boetcher-Joeres and Elizabeth Mittman in <span class="publication">Woolf Studies Annual</span>, Vol. 3 (Pace UP: 1997), pp. 254-57.`,
+    `<span class="publication">Radical Shelley: The Philosophical Anarchism and Utopian Thought of Percy Bysshe Shelley </span>, Michael Henry Scrivener (Princeton: Princeton UP, 1983), in <span class="publication">Kritikon Litterarum</span>, pp. 83-85.`,
+];
+export const lectures = [
+    `“Two Figures in Dense Violet Light,” Yale University, 1995.`,
+    `Multiple Presentations at the National Modern Language Association Conventions on panels and as panel chairs on topics including Milton and the Romantics; Feminism; Seventeenth Century Poetry, 1986-2001.`,
+    `Presentation at the Wordsworth Summer Conference, Grasmere, England, 1988. `,
+];
